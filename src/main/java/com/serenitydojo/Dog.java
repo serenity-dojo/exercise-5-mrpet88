3e9582560d2ae108545aaf9819df2fafd9f078b8
marinos.petratos <marinos.petratos@leaseplan.com>
2023-03-05 22:08:09 +0100

    refactor

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -1,26 +1,19 @@
 package com.serenitydojo;
 
-public class Dog {
-    private String name;
+public class Dog extends Pet {
     private String favoriteToy;
-    private int age;
+
     private boolean isFed = false;
 
     public static final String DOG_NOISE = "Woof";
 
     public Dog(String name, String favoriteToy, int age) {
-        this.name = name;
+        super(name, age);
         this.favoriteToy = favoriteToy;
-        this.age = age;
+
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getFavoriteToy() {
         return favoriteToy;
@@ -30,13 +23,7 @@ public class Dog {
         this.favoriteToy = favoriteToy;
     }
 
-    public int getAge() {
-        return age;
-    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public boolean isFed() {
         return isFed;
@@ -47,5 +34,9 @@ public class Dog {
 
     public void feed() {
         this.isFed = true;
+    }
+
+    public String play(){
+        return "plays with bone";
     }
 }
